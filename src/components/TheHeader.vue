f6ed98014bf8efa0330c9224e9f0a3f1b9c560f5
gab-che <gabriellachessa1@gmail.com>
2022-12-02 17:13:28 +0100

    completed header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,7 +2,7 @@
     export default{
         data(){
             return{
-
+                link_list: ["characters", "comics", "movies", "tv", "games", "collectibles", "videos", "fans", "news", "shop"],
             }
         },
 
@@ -13,9 +13,42 @@
 </script>
 
 <template>
+    <header>
+        <div class="container py-5">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="" class="img-fluid">
+            </div>
 
+            <div class="navbar">
+                <ul class="list-unstyled">
+                    <li v-for="link in link_list"><a href="#">{{link}}</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *;
+    
+    .container{
+        @include flex_space_between;
+
+        ul{
+            @include flex_space_between;
+            gap: 1rem;
+            text-transform: uppercase;
+            margin-bottom: 0;
+        }
+
+        a{
+            text-decoration: none;
+            padding: .5rem;
+            color: $primary_color;
+        }
+    }
+    .logo{
+        width: 50px;
+    }
 
 </style>
