fbf391dc7405cda7b6b5c7001981bb479836f3ec
gab-che <gabriellachessa1@gmail.com>
2022-12-02 17:19:30 +0100

    adds main bg color

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@
 
 <template>
   <TheHeader></TheHeader>
+  <MainContent></MainContent>
 </template>
 
 <style lang="scss">
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -13,9 +13,18 @@
 </script>
 
 <template>
-
+    <main>
+        <div class="container py-4">
+            <p>Insert content</p>
+        </div>
+    </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *;
 
+    main{
+        background-color: $primary_color;
+        color: $accent_color_light;
+    }
 </style>
