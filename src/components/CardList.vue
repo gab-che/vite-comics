bc031b1e29194d73eb51021b758cc899bb0f0fce
gab-che <gabriellachessa1@gmail.com>
2022-12-05 16:13:22 +0100

    links array via props

---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script>
+    import SingleCard from './SingleCard.vue';
+    export default{
+        components: { SingleCard },
+
+        props: {
+            cards: Array
+        },
+
+        data(){
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *;
+
+</style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,15 @@
+<script>
+    export default{
+        data(){
+            return{
+            }
+        }
+    }
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *;
+    
+</style>
